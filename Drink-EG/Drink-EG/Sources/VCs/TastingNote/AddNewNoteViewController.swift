573a36cfecc75f5739455a3ff6fcea8b9a00d0bc
SOOHYUNLEE08 <lsoo0827@gmail.com>
2024-07-31 17:16:28 +0900

    Feat: 테이스팅 노트 작성 및 와인 검색 UI 구현

--- Drink-EG/Drink-EG/Sources/VCs/TastingNote/AddNewNoteViewController.swift
+++ Drink-EG/Drink-EG/Sources/VCs/TastingNote/AddNewNoteViewController.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+class AddNewNoteViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    let wineSearchBar = UITextField()
+    let suggestionTableView = UITableView()
+    var suggestion: [String] = []
+    var allSuggestion: [String] = ["Apple", "Banana", "Grape", "Orange", "Watermelon", "Strawberry"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupView()
+        setupWineSearchBar()
+        setupWineSearchBarConstraints()
+        setupSuggestionTableView()
+        setupSuggestionTableViewConstraints()
+    }
+    
+    func setupView() {
+        view.addSubview(wineSearchBar)
+        view.addSubview(suggestionTableView)
+    }
+    
+    func setupWineSearchBar() {
+        wineSearchBar.delegate = self
+        wineSearchBar.placeholder = "와인 이름 입력 검색"
+        wineSearchBar.backgroundColor = .lightGray
+        wineSearchBar.tintColor = .blue
+    }
+    
+    func setupWineSearchBarConstraints() {
+        wineSearchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-10)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func setupSuggestionTableView() {
+        suggestionTableView.dataSource = self
+        suggestionTableView.delegate = self
+        suggestionTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func setupSuggestionTableViewConstraints() {
+        suggestionTableView.snp.makeConstraints{ make in
+            make.top.equalTo(wineSearchBar.snp.bottom).offset(0)
+            make.width.equalTo(wineSearchBar.snp.width)
+            make.height.equalTo(200)
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            let currentText = (textField.text as NSString?)?.replacingCharacters(in: range, with: string) ?? string
+            filterSuggestions(with: currentText)
+            return true
+        }
+
+    func filterSuggestions(with query: String) {
+        if query.isEmpty {
+                suggestion = []
+        } else {
+            suggestion = allSuggestion.filter { $0.lowercased().contains(query.lowercased()) }
+        }
+        suggestionTableView.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return suggestion.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = suggestion[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedSuggestion = suggestion[indexPath.row]
+        wineSearchBar.text = selectedSuggestion
+        suggestion = []
+        suggestionTableView.reloadData()
+    }
+}
--- Drink-EG/Drink-EG/Sources/VCs/TastingNote/WriteNoteViewController.swift
+++ Drink-EG/Drink-EG/Sources/VCs/TastingNote/WriteNoteViewController.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+class WriteNoteViewController: UIViewController {
+    let tasteSelect = UIStackView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupView()
+        setupTasteSelectStackView()
+        setupTasteSelectStackViewConstraints()
+    }
+    
+    func setupView() {
+        view.addSubview(tasteSelect)
+    }
+
+    func setupTasteSelectStackView() {
+        tasteSelect.axis = .vertical
+        tasteSelect.spacing = 20
+        tasteSelect.addArrangedSubview(createRow(labelText: "당도", lowText: "dry", highText: "sweet"))
+        tasteSelect.addArrangedSubview(createRow(labelText: "산도", lowText: "low", highText: "high"))
+        tasteSelect.addArrangedSubview(createRow(labelText: "타닌", lowText: "low", highText: "high"))
+        tasteSelect.addArrangedSubview(createRow(labelText: "바디", lowText: "light", highText: "bold"))
+        tasteSelect.addArrangedSubview(createRow(labelText: "알코올", lowText: "low", highText: "high"))
+    }
+    
+    func setupTasteSelectStackViewConstraints() {
+        tasteSelect.snp.makeConstraints{ make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-20)
+        }
+    }
+    
+    func createRow(labelText: String, lowText: String, highText: String) -> UIStackView {
+        let rowStackView = UIStackView()
+        rowStackView.axis = .horizontal
+        rowStackView.spacing = 10
+            
+        let label = UILabel()
+        label.text = labelText
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.backgroundColor = .white
+        label.textAlignment = .center
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        label.snp.makeConstraints { make in
+            make.width.equalTo(80)
+            make.height.equalTo(40)
+        }
+            
+        let lowLabel = UILabel()
+        lowLabel.text = lowText
+        lowLabel.font = UIFont.systemFont(ofSize: 12)
+            
+        let highLabel = UILabel()
+        highLabel.text = highText
+        highLabel.font = UIFont.systemFont(ofSize: 12)
+            
+        rowStackView.addArrangedSubview(label)
+        rowStackView.addArrangedSubview(lowLabel)
+            
+        for _ in 1...5 {
+            let button = UIButton()
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.black.cgColor
+            button.snp.makeConstraints { make in
+                make.width.height.equalTo(30)
+        }
+            rowStackView.addArrangedSubview(button)
+        }
+            
+        rowStackView.addArrangedSubview(highLabel)
+            
+        return rowStackView
+    }
+}
