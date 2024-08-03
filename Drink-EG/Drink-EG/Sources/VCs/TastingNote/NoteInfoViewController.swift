cb17ed2ea9f23ee78fda6d6643dbfd57478716c1
SOOHYUNLEE08 <lsoo0827@gmail.com>
2024-08-03 13:34:32 +0900

    Feat: 사용자 선택을 Pentagon으로 보여주는 UI 구현

--- Drink-EG/Drink-EG/Sources/VCs/TastingNote/NoteInfoViewController.swift
+++ Drink-EG/Drink-EG/Sources/VCs/TastingNote/NoteInfoViewController.swift
@@ -6,3 +6,169 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+class NoteInfoViewController: UIViewController {
+    
+    let pentagonChart = PolygonChartView()
+    let wineView = UIView()
+    let wineImageView = UIImageView()
+    let wineName = UILabel()
+    let chooseColorView = UIView()
+    let chooseColorLabel = UILabel()
+    let chooseColorButtons = [UIButton(), UIButton(), UIButton(), UIButton(), UIButton(), UIButton()]
+    var dataList: [RadarChartData] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupWineView()
+        setupWineViewConstraints()
+        setupWineImageView()
+        setupWineImageViewConstraints()
+        setupWineName()
+        setupWineNameConstraints()
+        setupPentagonChart()
+        setupPentagonChartConstraints()
+        setupChooseColorView()
+        setupChooseColorViewConstraints()
+        setupChooseColorLabel()
+        setupChooseColorLabelConstraints()
+        setupColorButton()
+        setupColorButtonConstraints()
+    }
+    
+    func setupPentagonChart() {
+        view.addSubview(pentagonChart)
+        pentagonChart.dataList = dataList
+        pentagonChart.layer.cornerRadius = 10
+        pentagonChart.addTarget(self, action: #selector(pentagonButtonTapped), for: .touchUpInside)
+    }
+    
+    func setupPentagonChartConstraints() {
+        pentagonChart.snp.makeConstraints{ make in
+            make.top.equalTo(wineView.snp.bottom).offset(10)
+            make.centerX.equalTo(view.safeAreaLayoutGuide.snp.centerX)
+            make.width.equalTo(361)
+            make.height.equalTo(349)
+        }
+    }
+    
+    func setupWineView() {
+        view.addSubview(wineView)
+        wineView.backgroundColor = UIColor(hex: "FFD73880")
+        wineView.layer.cornerRadius = 10
+    }
+    
+    func setupWineViewConstraints() {
+        wineView.snp.makeConstraints{ make in
+            make.top.equalTo(view.snp.top).offset(156)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(361)
+            make.height.equalTo(94)
+        }
+    }
+    
+    func setupWineImageView() {
+        wineView.addSubview(wineImageView)
+        wineImageView.contentMode = .scaleAspectFit
+        wineImageView.layer.cornerRadius = 10
+        wineImageView.layer.masksToBounds = true
+        wineImageView.image = UIImage(named: "SampleImage")
+    }
+    
+    func setupWineImageViewConstraints() {
+        wineImageView.snp.makeConstraints{ make in
+            make.leading.equalTo(wineView.snp.leading).offset(8)
+            make.top.equalTo(wineView.snp.top).offset(7)
+            make.bottom.equalTo(wineView.snp.bottom).offset(-7)
+            make.width.height.equalTo(80)
+            
+        }
+    }
+    
+    func setupWineName() {
+        wineView.addSubview(wineName)
+        wineName.text = "19 Crhnes"
+        
+    }
+    
+    func setupWineNameConstraints() {
+        wineName.snp.makeConstraints{ make in
+            make.centerY.equalTo(wineImageView.snp.centerY)
+            make.leading.equalTo(wineImageView.snp.trailing).offset(25)
+            make.top.equalTo(wineView.snp.top).offset(36)
+            make.bottom.equalTo(wineView.snp.bottom).offset(-36)
+        }
+    }
+    
+    func setupChooseColorView() {
+        view.addSubview(chooseColorView)
+        chooseColorView.backgroundColor = .white
+        chooseColorView.layer.borderWidth = 2
+        chooseColorView.layer.cornerRadius = 10
+        chooseColorView.layer.borderColor = UIColor(hex: "0000001A")?.cgColor
+    }
+    
+    func setupChooseColorViewConstraints() {
+        chooseColorView.snp.makeConstraints{ make in
+            make.top.equalTo(pentagonChart.snp.bottom).offset(10)
+            make.centerX.equalTo(wineView.snp.centerX)
+            make.width.equalTo(361)
+            make.height.equalTo(317)
+        }
+    }
+    
+    func setupChooseColorLabel() {
+        chooseColorView.addSubview(chooseColorLabel)
+        chooseColorLabel.text = "색상 선택"
+        chooseColorLabel.textAlignment = .center
+        chooseColorLabel.textColor = .black
+    }
+    
+    func setupChooseColorLabelConstraints() {
+        chooseColorLabel.snp.makeConstraints{ make in
+            make.leading.equalTo(chooseColorView.snp.leading).offset(19)
+            make.top.equalTo(chooseColorView.snp.top).offset(27)
+            make.width.equalTo(119)
+            make.height.equalTo(35)
+        }
+    }
+    
+    func setupColorButton() {
+        for i in chooseColorButtons {
+            chooseColorView.addSubview(i)
+            i.layer.cornerRadius = 10
+        }
+        
+        chooseColorButtons[0].backgroundColor = UIColor(hex: "FFFBD8")
+        chooseColorButtons[1].backgroundColor = UIColor(hex: "FDEFA4")
+        chooseColorButtons[2].backgroundColor = UIColor(hex: "FEB6B6")
+        chooseColorButtons[3].backgroundColor = UIColor(hex: "BA2121")
+        chooseColorButtons[4].backgroundColor = UIColor(hex: "892222")
+        chooseColorButtons[5].backgroundColor = UIColor(hex: "521515")
+    }
+    
+    func setupColorButtonConstraints() {
+        for i in 0..<chooseColorButtons.count {
+            let button = chooseColorButtons[i]
+            button.snp.makeConstraints{ make in
+                make.top.equalTo(chooseColorLabel.snp.bottom).offset(10)
+                make.width.height.equalTo(40)
+                if i == 0 {
+                    make.leading.equalTo(chooseColorView.snp.leading).offset(19)
+                } else {
+                    make.leading.equalTo(chooseColorButtons[i-1].snp.trailing).offset(8)
+                }
+            }
+        }
+    }
+    
+    @objc func pentagonButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
+}
--- Drink-EG/Drink-EG/Sources/VCs/TastingNote/WriteNoteViewController.swift
+++ Drink-EG/Drink-EG/Sources/VCs/TastingNote/WriteNoteViewController.swift
@@ -4,17 +4,115 @@ import SnapKit
 class WriteNoteViewController: UIViewController {
     
     let tastingnoteLabel = UILabel()
+    let categoriesView = UIView()
+    let wineView = UIView()
+    let wineImageView = UIImageView()
+    let wineName = UILabel()
+    let showPentagonButton = UIButton()
     let categories = ["당도", "산도", "타닌", "바디", "알코올"]
     var categoryLabels: [UILabel] = []
     var buttonGroups: [[UIButton]] = []
     var connectingLinesGroups: [[UIView]] = []
+    var selectedValues: [CharacteristicType: Int] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(hex: "EAEAEA")
+        view.backgroundColor = .white
         
+        setupWineView()
+        setupWineViewConstraints()
+        setupWineImageView()
+        setupWineImageViewConstraints()
+        setupShowPentagonButton()
+        setupShowPentagonButtonConstraints()
+        setupWineName()
+        setupWineNameConstraints()
         setupCategories()
         setupConstraints()
+        setupCatagoriesView()
+        setupCategoriesViewConstraints()
+        showPentagonButton.isHidden = true
+    }
+    
+    func setupWineView() {
+        view.addSubview(wineView)
+        wineView.backgroundColor = UIColor(hex: "FFD73880")
+        wineView.layer.cornerRadius = 10
+    }
+    
+    func setupWineViewConstraints() {
+        wineView.snp.makeConstraints{ make in
+            make.top.equalTo(view.snp.top).offset(156)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(361)
+            make.height.equalTo(94)
+        }
+    }
+    
+    func setupWineImageView() {
+        wineView.addSubview(wineImageView)
+        wineImageView.contentMode = .scaleAspectFit
+        wineImageView.layer.cornerRadius = 10
+        wineImageView.layer.masksToBounds = true
+        wineImageView.image = UIImage(named: "SampleImage")
+    }
+    
+    func setupWineImageViewConstraints() {
+        wineImageView.snp.makeConstraints{ make in
+            make.leading.equalTo(wineView.snp.leading).offset(8)
+            make.top.equalTo(wineView.snp.top).offset(7)
+            make.bottom.equalTo(wineView.snp.bottom).offset(-7)
+            make.width.height.equalTo(80)
+            
+        }
+    }
+    
+    func setupWineName() {
+        wineView.addSubview(wineName)
+        wineName.text = "19 Crhnes"
+        
+    }
+    
+    func setupWineNameConstraints() {
+        wineName.snp.makeConstraints{ make in
+            make.centerY.equalTo(wineImageView.snp.centerY)
+            make.leading.equalTo(wineImageView.snp.trailing).offset(25)
+            make.top.equalTo(wineView.snp.top).offset(36)
+            make.bottom.equalTo(wineView.snp.bottom).offset(-36)
+        }
+    }
+    
+    func setupShowPentagonButton() {
+        categoriesView.addSubview(showPentagonButton)
+        showPentagonButton.layer.cornerRadius = 10
+        showPentagonButton.setTitle("Next", for: .normal)
+        showPentagonButton.setTitleColor(.black, for: .normal)
+        showPentagonButton.backgroundColor = UIColor(hex: "FFD73880")
+        showPentagonButton.addTarget(self, action: #selector(showPentagonButtonTapped), for: .touchUpInside)
+    }
+    
+    func setupShowPentagonButtonConstraints() {
+        showPentagonButton.snp.makeConstraints{ make in
+            make.bottom.equalTo(categoriesView.snp.bottom).offset(-20)
+            make.centerX.equalTo(categoriesView.snp.centerX)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func setupCatagoriesView() {
+        view.addSubview(categoriesView)
+        categoriesView.backgroundColor = UIColor(hex: "EAEAEA")
+        categoriesView.layer.cornerRadius = 10
+    }
+    
+    func setupCategoriesViewConstraints() {
+        categoriesView.snp.makeConstraints{ make in
+            make.top.equalTo(wineView.snp.bottom).offset(10)
+            make.centerX.equalTo(view.snp.centerX)
+            make.width.equalTo(361)
+            make.height.equalTo(543)
+        }
     }
     
     func setupCategories() {
@@ -27,17 +125,17 @@ class WriteNoteViewController: UIViewController {
             label.layer.cornerRadius = 10
             label.layer.masksToBounds = true
             categoryLabels.append(label)
-            view.addSubview(label)
+            categoriesView.addSubview(label)
             
             var buttons: [UIButton] = []
             for buttonIndex in 0..<5 {
                 let button = UIButton()
-                button.layer.cornerRadius = 10
+                button.layer.cornerRadius = 8
                 button.backgroundColor = UIColor(hex: "B3B3B3")
                 button.tag = categoryIndex * 5 + buttonIndex
                 button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
                 buttons.append(button)
-                view.addSubview(button)
+               categoriesView.addSubview(button)
             }
             buttonGroups.append(buttons)
             
@@ -46,7 +144,7 @@ class WriteNoteViewController: UIViewController {
                 let line = UIView()
                 line.backgroundColor = UIColor.gray
                 connectingLines.append(line)
-                view.addSubview(line)
+                categoriesView.addSubview(line)
             }
             connectingLinesGroups.append(connectingLines)
         }
@@ -59,13 +157,13 @@ class WriteNoteViewController: UIViewController {
             let connectingLines = connectingLinesGroups[i]
             
             label.snp.makeConstraints { make in
-                make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(20)
-                make.width.equalTo(70)
-                make.height.equalTo(40)
+                make.width.equalTo(71)
+                make.height.equalTo(39)
+                make.leading.equalTo(categoriesView.snp.leading).offset(17)
                 if i == 0 {
-                    make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(100)
+                    make.top.equalTo(categoriesView.snp.top).offset(23)
                 } else {
-                    make.top.equalTo(categoryLabels[i - 1].snp.bottom).offset(75)
+                    make.top.equalTo(categoryLabels[i-1].snp.bottom).offset(48)
                 }
             }
             
@@ -73,11 +171,12 @@ class WriteNoteViewController: UIViewController {
                 button.snp.makeConstraints { make in
                     make.centerY.equalTo(label.snp.centerY)
                     if index == 0 {
-                        make.leading.equalTo(label.snp.trailing).offset(20)
+                        make.leading.equalTo(categoryLabels[i].snp.trailing).offset(15)
                     } else {
-                        make.leading.equalTo(buttons[index - 1].snp.trailing).offset(20)
+                        make.leading.equalTo(buttons[index - 1].snp.trailing).offset(40)
                     }
-                    make.width.height.equalTo(20)
+                    make.width.equalTo(16)
+                    make.height.equalTo(16)
                 }
                 
                 if index < buttons.count - 1 {
@@ -92,14 +191,38 @@ class WriteNoteViewController: UIViewController {
         }
     }
     
+    func checkIfAllSelected() {
+        if selectedValues.count == categories.count {
+            showPentagonButton.isHidden = false
+        } else {
+            showPentagonButton.isHidden = true
+        }
+    }
+    
     @objc func buttonTapped(_ sender: UIButton) {
         let tag = sender.tag % 5 // 각 줄에서 몇 번째 버튼인지 계산
         let row = sender.tag / 5 // 몇 번째 줄인지 계산
-        print("Row: \(row + 1), Button: \(tag + 1)번째 버튼")
+        print("Row: \(row + 1), Button: \((tag + 1)*20)")
         
         for button in buttonGroups[row] {
             button.backgroundColor = .lightGray
         }
         sender.backgroundColor = .red
+        
+        let selectedValue = (tag + 1) * 20
+        let characteristic = categories[row]
+        selectedValues[CharacteristicType(rawValue: characteristic)!] = selectedValue
+        
+        checkIfAllSelected()
+    }
+    
+    @objc func showPentagonButtonTapped() {
+        let polygonVC = NoteInfoViewController()
+        var dataList: [RadarChartData] = []
+        for (type, value) in selectedValues {
+            dataList.append(RadarChartData(type: type, value: value))
+        }
+        polygonVC.dataList = dataList
+        navigationController?.pushViewController(polygonVC, animated: true)
     }
 }
