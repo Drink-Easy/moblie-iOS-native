d9ee77a34903b2ab5ec51af4dba7f28d632f0b0a
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-08-12 14:18:03 +0900

    Fix : Merge conflict

--- Drink-EG/Drink-EG/Resources/APIs/Example/BasicAPI.swift
+++ Drink-EG/Drink-EG/Resources/APIs/Example/BasicAPI.swift
@@ -11,6 +11,7 @@ import Moya
 // JWT 토큰을 받을 수 있도록 BasicAPI 정의
 /// 기능 하나 당 API 1개씩 만들기
 /// - API 명세서 기준으로 1 명세서 당 1 API enum 정의하기
+/// - 변경을 한 번 더 해보자.
 
 enum BasicAPI {
     case getUser(id: Int)
