7cd155b880ee688d76264ee2a24c38b782f34ead
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-08-16 00:03:22 +0900

    #43 Chore: Sliding Cards Deck UI package 추가

---
+++ Drink-EG/Drink-EG/Sources/VCs/WineClass/WineKnowledgeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WineKnowledgeViewController.swift
+//  Drink-EG
+//
+//  Created by 김도연 on 8/15/24.
+//
+
+import UIKit
+import SnapKit
+
+class WineKnowledgeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "와인 지식"
+    }
+}
