0f74b34372029cb200d2e2eacb0fc284d55152fd
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-08-17 03:47:36 +0900

    Merge

---
+++ Drink-EG/Drink-EG/Sources/VCs/TasteTest/ThirdVarietyTasteTestViewController 2.swift
@@ -0,0 +1,242 @@
+//
+//  ThirdVarietyTasteTestViewController.swift
+//  Drink-EG
+//
+//  Created by 이현주 on 8/13/24.
+//
+
+import UIKit
+import Moya
+ 
+class ThirdVarietyTasteTestViewController: UIViewController {
+    var variety: [String] = ["까베르네소비뇽", "샤도네이", "메를로", "까베르네프랑", "피노누아", "쉬라즈", "쇼비뇽 블랑", "그르나슈", "말벡", "산지오베제", "모스카토", "리슬링", "템프라니요", "네비올로", "블랜드", "쁘띠베르도", "무르베드르", "카르메너르", "기타"]
+    var selectedIndexPaths: [IndexPath] = []
+    private var selectedVariety : [String] = []
+    var memberInfoDTO : MemberInfoRequest?
+    
+    let startButton = UIButton(type: .system)
+    
+    private let titleLabel: UILabel = {
+        let l = UILabel()
+        l.text = "제 와인 취향은..."
+        l.textColor = .black
+        l.textAlignment = .left
+        l.font = UIFont.systemFont(ofSize: 32, weight: UIFont.Weight(rawValue: 700))
+        
+        return l
+    }()
+    
+    private let kindLabel: UILabel = {
+        let l = UILabel()
+        l.text = "품종 (복수선택 가능)"
+        l.textColor = .black
+        l.textAlignment = .left
+        l.font = UIFont.boldSystemFont(ofSize: 20)
+        return l
+    }()
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        self.navigationController?.isNavigationBarHidden = false
+//        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+//        self.navigationController?.navigationBar.tintColor = .black
+        
+        view.backgroundColor = .white
+        setupUI()
+    }
+    
+    lazy var VarietyCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.register(TasteTestThirdCollectionViewCell.self, forCellWithReuseIdentifier: "TasteTestThirdCollectionViewCell")
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.showsVerticalScrollIndicator = false
+        cv.delegate = self
+        cv.dataSource = self
+        
+        cv.decelerationRate = .fast
+        cv.backgroundColor = .clear
+        cv.layer.cornerRadius = 10
+        
+        return cv
+    }()
+    
+    private func setupUI() {
+        configureStartButton()
+        
+        view.addSubview(startButton)
+        startButton.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(727)
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(view).offset(33)
+            make.height.equalTo(60)
+            make.width.equalTo(327)
+        }
+        
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(125)
+            make.leading.equalToSuperview().offset(18)
+            make.width.equalTo(338)
+            make.height.equalTo(44)
+        }
+        
+        view.addSubview(kindLabel)
+        kindLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(30)
+            make.leading.equalTo(titleLabel)
+        }
+        
+        view.addSubview(VarietyCollectionView)
+        VarietyCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(kindLabel.snp.bottom).offset(30)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(31)
+            make.height.greaterThanOrEqualTo(443)
+        }
+    }
+    
+    private func configureStartButton() {
+        startButton.setTitle("시작하기", for: .normal)
+        startButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        startButton.setTitleColor(.white, for: .normal)
+        startButton.contentHorizontalAlignment = .center
+        
+        startButton.backgroundColor = UIColor(hex: "#E2E2E2")
+        startButton.layer.cornerRadius = 16
+        startButton.layer.borderWidth = 0
+    }
+    
+    private func updateStartButtonState() {
+        if selectedIndexPaths.isEmpty {
+            // 선택된 셀이 없는 경우
+            startButton.isEnabled = false
+            startButton.backgroundColor = UIColor(hex: "#E2E2E2")
+            startButton.removeTarget(nil, action: nil, for: .allEvents)
+        } else {
+            // 선택된 셀이 하나 이상 있는 경우
+            startButton.setTitleColor(.white, for: .normal)
+            startButton.tintColor = .white
+            startButton.isEnabled = true
+            startButton.backgroundColor = UIColor(hex: "FA735B")
+            startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+            
+            SelectionManager.shared.setWineVariety(anser: selectedVariety)
+        }
+    }
+    
+    @objc private func startButtonTapped() {
+        assignRequestDTO()
+        callAPI() { [weak self] isSuccess in
+            if isSuccess {
+                self?.goToHomeView()
+            } else {
+                print("유저 정보 전송 실패")
+            }
+        }
+    }
+    
+    private func goToHomeView() {
+        let mainTabBarViewController = MainTabBarViewController()
+        navigationController?.pushViewController(mainTabBarViewController, animated: true)
+    }
+    
+    //MARK: - API parts
+    let provider = MoyaProvider<MemberInfoAPI>(plugins: [CookiePlugin()])
+    
+    func assignRequestDTO() {
+            let selectionMng = SelectionManager.shared
+            self.memberInfoDTO = MemberInfoRequest(
+                isNewbie: selectionMng.isNewbie,
+                monthPrice: selectionMng.monthPrice,
+                wineSort: selectionMng.wineSort,
+                wineArea: selectionMng.wineNation,
+                wineVariety: selectionMng.wineVariety,
+                region: selectionMng.userAddr,
+                name: selectionMng.userName
+            )
+        }
+        
+        func callAPI(completion: @escaping (Bool) -> Void) {
+            if let data = self.memberInfoDTO {
+                provider.request(.patchMember(data: data)) { result in
+                    switch result {
+                    case .success(let response):
+                        do {
+                            let data = try response.map(APIResponseMemberResponse.self)
+                            print("Success: \(data)")
+                            LoginViewController.isFirstLogin = false
+                            completion(data.isSuccess)
+                        } catch {
+                            print("Failed to map data: \(error)")
+                            completion(false)
+                        }
+                    case .failure(let error):
+                            print("Request failed: \(error)")
+                            completion(false)
+                    }
+                }
+            } else {
+                print("유저 선택 정보가 없습니다.")
+                completion(false)
+            }
+        }
+    
+}
+
+extension ThirdVarietyTasteTestViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return variety.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TasteTestThirdCollectionViewCell", for: indexPath) as! TasteTestThirdCollectionViewCell
+            
+        cell.configure(name: variety[indexPath.item])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? TasteTestThirdCollectionViewCell {
+            if cell.View.backgroundColor == .white || cell.View.backgroundColor == UIColor(hex: "E6E6E6") {
+                // 셀이 선택되지 않은 상태였을 때
+                cell.View.backgroundColor = UIColor(hex: "FBCBC4")
+                cell.View.layer.borderColor = UIColor(hex: "FA8D7B")?.cgColor  // 원하는 색으로 변경
+                
+                // 선택된 셀의 indexPath를 배열에 추가
+                selectedIndexPaths.append(indexPath)
+                selectedVariety.append(cell.name.text ?? "")
+            } else {
+                // 셀이 이미 선택된 상태였을 때 (다시 클릭하면 원래대로)
+                if (cell.name.text == "기타") {
+                    cell.View.backgroundColor = UIColor(hex: "E6E6E6")
+                } else {
+                    cell.View.backgroundColor = .white
+                }
+                cell.View.layer.borderColor = UIColor(hex: "C3C3C3")?.cgColor
+                    
+                // 선택 해제된 셀의 indexPath를 배열에서 제거
+                if let index = selectedIndexPaths.firstIndex(of: indexPath) {
+                    selectedIndexPaths.remove(at: index)
+                }
+                selectedVariety = selectedVariety.filter{$0 != (cell.name.text ?? "")}
+            }
+                
+            // nextButton의 상태 업데이트
+            updateStartButtonState()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 33)
+    }
+}
---
+++ Drink-EG/Drink-EG/Sources/VCs/TasteTest/WhatsURNameViewController 2.swift
@@ -0,0 +1,209 @@
+//
+//  WhatsURNameViewController.swift
+//  Drink-EG
+//
+//  Created by 이현주 on 8/13/24.
+//
+
+import UIKit
+import Moya
+import CoreLocation
+
+class WhatsURNameViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDelegate {
+    let nextButton = UIButton(type: .system)
+    var locationManager = CLLocationManager()
+    
+    
+    private let titleLabel: UILabel = {
+        let l = UILabel()
+        l.text = "제 이름은..."
+        l.textColor = .black
+        l.textAlignment = .left
+        l.font = UIFont.systemFont(ofSize: 32, weight: UIFont.Weight(rawValue: 700))
+        
+        return l
+    }()
+    
+    private let nameTextField: UITextField = {
+        let t = UITextField()
+        t.layer.cornerRadius = 16
+        t.layer.masksToBounds = true
+        t.backgroundColor = UIColor(hex: "E2E2E2")
+        t.placeholder = "입력..."
+        t.returnKeyType = .done
+        t.clearButtonMode = .always
+        t.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 16.0, height: 0.0))
+        t.leftViewMode = .always
+        return t
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        self.navigationController?.navigationBar.tintColor = .black
+        
+        nameTextField.delegate = self
+        
+        // nameTextField의 값이 변경될 때 updateNextButtonState를 호출하도록 설정
+        nameTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+        // 초기 상태 업데이트
+        updateNextButtonState()
+        
+        // Location
+        setupLocationManager()
+        
+        view.backgroundColor = .white
+        setupUI()
+    }
+    
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        updateNextButtonState()
+    }
+    
+    private func setupUI() {
+        configureNextButton()
+        
+        view.addSubview(nextButton)
+        nextButton.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(727)
+            make.centerX.equalToSuperview()
+            make.leading.equalTo(view).offset(33)
+            make.height.equalTo(60)
+            make.width.equalTo(327)
+        }
+        
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(125)
+            make.leading.equalToSuperview().offset(18)
+            make.width.equalTo(338)
+            make.height.equalTo(44)
+        }
+        
+        view.addSubview(nameTextField)
+        nameTextField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(46)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(17)
+            make.height.greaterThanOrEqualTo(60)
+        }
+    }
+    
+    private func configureNextButton() {
+        nextButton.setTitle("다음", for: .normal)
+        nextButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        nextButton.setTitleColor(.white, for: .normal)
+        nextButton.contentHorizontalAlignment = .center
+        
+        nextButton.setImage(UIImage(named: "icon_next"), for: .normal)
+        nextButton.imageView?.contentMode = .center
+        nextButton.tintColor = .white
+        nextButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 105, bottom: 0, right: 0)
+        
+        nextButton.backgroundColor = UIColor(hex: "#E2E2E2")
+        nextButton.layer.cornerRadius = 16
+        nextButton.layer.borderWidth = 0
+    }
+    
+    private func updateNextButtonState() {
+        if let name = nameTextField.text {
+            if name.isEmpty { // 이름 입력 안된 경우
+                nextButton.isEnabled = false
+                nextButton.backgroundColor = UIColor(hex: "#E2E2E2")
+                nextButton.removeTarget(nil, action: nil, for: .allEvents)
+            } else {
+                nextButton.setTitleColor(.white, for: .normal)
+                nextButton.tintColor = .white
+                nextButton.isEnabled = true
+                nextButton.backgroundColor = UIColor(hex: "FA735B")
+                nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+                
+                // 이름 정보 전달
+                SelectionManager.shared.setName(answer: name)
+            }
+        }
+    }
+    
+    @objc private func nextButtonTapped() {
+        let firstTasteTestViewController = FirstTasteTestViewController()
+        navigationController?.pushViewController(firstTasteTestViewController, animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.nameTextField {
+            self.nameTextField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)  //firstresponder가 전부 사라짐
+    }
+    
+    //MARK: - Location parts
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            self.locationManager.startUpdatingLocation()
+        case .denied, .restricted:
+            print("위치 서비스가 제한 또는 거부되었습니다.")
+            self.locationManager.stopUpdatingLocation()
+        case .notDetermined:
+            print("위치 권한이 아직 결정되지 않았습니다.")
+            self.locationManager.requestWhenInUseAuthorization()
+        @unknown default:
+            print("알 수 없는 권한 상태입니다.")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        
+        // 위치 정보를 기반으로 주소를 가져옴
+        reverseGeocode(location: location)
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    private func reverseGeocode(location: CLLocation) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            if let error = error {
+                print("역지오코딩 실패: \(error)")
+                return
+            }
+            if let placemark = placemarks?.first {
+                var address = ""
+                
+                if let administrativeArea = placemark.administrativeArea {
+                    address += administrativeArea
+                }
+                
+                if let locality = placemark.locality {
+                    address += " \(locality)"
+                }
+                
+                if let subLocality = placemark.subLocality {
+                    address += " \(subLocality)"
+                }
+                
+                if let thoroughfare = placemark.thoroughfare {
+                    address += " \(thoroughfare)"
+                }
+                
+                SelectionManager.shared.userAddr = address
+            }
+        }
+    }
+}
+
