0bf369c64a7a1ebf94e7a0da36d106e7de2c0de0
Hyeon-Ju <lhjivy1024@gmail.com>
2024-08-20 14:14:31 +0900

    마이페이지 수정

--- Drink-EG/Drink-EG/Sources/VCs/Settings/MypageViewController.swift
+++ Drink-EG/Drink-EG/Sources/VCs/Settings/MypageViewController.swift
@@ -37,6 +37,11 @@ class MypageViewController: UIViewController {
         showBadge()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     // title label
     let mypageLabel: UILabel = {
         let label = UILabel()
@@ -204,25 +209,26 @@ extension MypageViewController: UITableViewDelegate {
                 bottomSheetVC.modalPresentationStyle = .overFullScreen
                 bottomSheetVC.modalTransitionStyle = .crossDissolve
                 present(bottomSheetVC, animated: true, completion: nil)
-            case 1:
-                let controller = MyPageSettingsViewController()
-                navigationController?.pushViewController(controller, animated: true)
+//            case 1:
+//                let controller = MyPageSettingsViewController()
+//                navigationController?.pushViewController(controller, animated: true)
             default:
                 print("\(myPagefirstMenu[indexPath.row])")
             }
         case 1:
             switch indexPath.row {
-            case 0:
-                let controller = MyPageSettingsViewController()
-                navigationController?.pushViewController(controller, animated: true)
+//            case 0:
+//                let controller = MyPageSettingsViewController()
+//                navigationController?.pushViewController(controller, animated: true)
             default:
                 print("\(myPageSecondMenu[indexPath.row])")
             }
         case 2:
             switch indexPath.row {
-            case 0:
-                let controller = MypageViewController()
-                navigationController?.pushViewController(controller, animated: true)
+//            case 0:
+//                
+//                let controller = MypageViewController()
+//                navigationController?.pushViewController(controller, animated: true)
             default:
                 print("\(myPageThirdMenu[indexPath.row])")
             }
