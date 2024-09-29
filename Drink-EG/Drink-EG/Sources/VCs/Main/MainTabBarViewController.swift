22d4c4309776f1a2fd85ea7c843e9d78ac21ecbf
Hyeon-Ju <lhjivy1024@gmail.com>
2024-09-29 14:40:08 +0900

    #82 Feat: 탭바 재구현 (아이콘 제외)

--- Drink-EG/Drink-EG/Sources/VCs/Main/MainTabBarViewController.swift
+++ Drink-EG/Drink-EG/Sources/VCs/Main/MainTabBarViewController.swift
@@ -18,7 +18,7 @@ class MainTabBarViewController : UITabBarController {
         super.viewDidLayoutSubviews()
         var tabFrame = tabBar.frame
         tabFrame.size.height = 93
-        tabFrame.origin.y = view.frame.size.height - 93
+        tabFrame.origin.y = view.frame.size.height - 90
         tabBar.frame = tabFrame
     }
     
@@ -30,20 +30,20 @@ class MainTabBarViewController : UITabBarController {
         let nav4 = UINavigationController(rootViewController: CommunityMainViewController())
         let nav5 = UINavigationController(rootViewController: MypageViewController())
         
-        nav1.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "TabHome"), tag: 0)
-        nav2.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "TabClass"), tag: 1)
-        nav3.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "TabNote"), tag: 2)
-        nav4.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "TabGroup"), tag: 3)
-        nav5.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "TabSetting"), tag: 4)
+        nav1.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "TabHome"), tag: 0)
+        nav2.tabBarItem = UITabBarItem(title: "클래스", image: UIImage(named: "TabClass"), tag: 1)
+        nav3.tabBarItem = UITabBarItem(title: "테이스팅 노트", image: UIImage(named: "TabNote"), tag: 2)
+        nav4.tabBarItem = UITabBarItem(title: "모임", image: UIImage(named: "TabGroup"), tag: 3)
+        nav5.tabBarItem = UITabBarItem(title: "설정", image: UIImage(named: "TabSetting"), tag: 4)
+        
+        UITabBar.clearShadow()
+        tabBar.layer.applyShadow(color: .gray, alpha: 0.3, x: 0, y: 0, blur: 15)
         
         tabBar.tintColor = .label
-        tabBar.backgroundColor = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: 0.6)
-        tabBar.layer.cornerRadius = 20
-        tabBar.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
-        tabBar.layer.masksToBounds = true
+        tabBar.backgroundColor = .white
         
-        tabBar.tintColor = .white
-        tabBar.unselectedItemTintColor = UIColor(hue: 0, saturation: 0, brightness: 0.85, alpha: 1.0)
+        tabBar.tintColor = UIColor(hex: "5813B1")
+        tabBar.unselectedItemTintColor = UIColor(hue: 0, saturation: 0, brightness: 0, alpha: 0.5)
         
         setViewControllers([nav1, nav2, nav3, nav4, nav5], animated: true)
     }
